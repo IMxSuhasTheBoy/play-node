db7bc6adaf08a89e32881f7df889b6acf7366d1f
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-05-24 21:52:56 +0530

    Update file name

--- play-http-server/server1.js
+++ play-http-server/server1.js
@@ -10,13 +10,15 @@ const server = http.createServer((req, res) => {
   // res.setHeader("Content-Type", "text/html");
   // res.statusCode = 404;
  */
+  console.log(req.method, req.url);
 
-  res.writeHead(500, { "Content-Type": "application/json" });
+  // res.writeHead(500, { "Content-Type": "application/json" });
+  res.writeHead(200, { "Content-Type": "text/html" });
   res.write("Hello World ");
-  // res.end('<h1>"Hello World "</h1>');
-  res.end(JSON.stringify({ message: "Server error! ! !" }));
+  res.end('<h1>"Hello World "</h1>');
+  // res.end(JSON.stringify({ message: "Server error! ! !" }));
 });
 
 server.listen(PORT || 8001, () => {
-  console.log(`Node http Server is running on port ${PORT}`);
+  console.log(`Node http Server1 is running on port ${PORT}`);
 });
