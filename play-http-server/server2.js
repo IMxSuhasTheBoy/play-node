a8ebcdd535342d78af1c3321b1481228e7ade19b
IMxSuhasTheBoy <dannylivecsr2@gmail.com>
2024-05-25 23:29:14 +0530

    Add file to exp static pages serving

--- play-http-server/server2.js
+++ play-http-server/server2.js
@@ -10,20 +10,23 @@ console.log(__filename, __dirname);
 
 const PORT = config.get("PORT");
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   //*simple routes
   try {
     if (req.method === "GET") {
+      let filePath;
       if (req.url === "/") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end('<h1>"server2 Home page"</h1>');
+        filePath = path.join(__dirname, "public", "index.html");
       } else if (req.url === "/about") {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end('<h1>"server2 About page"</h1>');
+        filePath = path.join(__dirname, "public", "about.html");
       } else {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.end('<h1>"server2 404 page"</h1>');
+        throw new Error("Page not found! ! !");
       }
+
+      const data = await fs.readFile(filePath);
+      res.setHeader("Content-Type", "text/html");
+      res.write(data);
+      res.end();
     } else {
       throw new Error("Invalid request! ! !");
     }
